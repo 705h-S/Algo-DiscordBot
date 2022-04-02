e5f419a2b2e18ddc1c4c2401300050ec8b310017
705H-S <93358442+705h-S@users.noreply.github.com>
2022-04-02 14:05:24 -0700

    added eventHandler for client and guild

--- Handlers/commandHandle.js
+++ Handlers/commandHandle.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 
 module.exports = (client, Discord)=>{
     const commandFile = fs.readdirSync(`./Commands/`).filter(file => file.endsWith('.js'));
-        // Looping through the commands, better than writing a single if statements for each command
+        // Looping through the commands, better than writing a single if statements for each commandc
     for(const file of commandFile){
         const command = require(`../Commands/${file}`);
         if(command.name){
