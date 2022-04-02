c2704e4cf34385f711b03eb067204a8bc0387f16
705H-S <93358442+705h-S@users.noreply.github.com>
2022-04-02 13:53:42 -0700

    added commandHandle to look into the commands folder and execute command

---
+++ Handlers/commandHandle.js
@@ -0,0 +1,14 @@
+const fs = require('fs');
+
+module.exports = (client, Discord)=>{
+    const commandFile = fs.readdirSync(`./Commands/`).filter(file => file.endsWith('.js'));
+        // Looping through the commands, better than writing a single if statements for each command
+    for(const file of commandFile){
+        const command = require(`../Commands/${file}`);
+        if(command.name){
+            client.commands.set(command.name, command);
+        }else{
+            continue;
+        }
+    }
+}
